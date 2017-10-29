9c099c28e3a7cfcc31519156383e9c2bc03c0942
hallokael <646058898@qq.com>
2017-10-29 14:35:38 +0800

    machine learning

---
+++ kNN.py
@@ -0,0 +1,494 @@
+from os import listdir
+from numpy import *
+from math import log
+import operator
+def createDataSet():
+    group = array([[1.0,1.1 ]  , [1.0,1.0], [0,0 ], [0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+def classifyO(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape [0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances .argsort ( )
+    classCount={}
+    for  i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0] [0]
+def file2matrix(filename):
+    fr=open(filename)
+    arrayOfLines=fr.readlines()
+    numberOfLines=len(arrayOfLines)
+    returnMat=zeros((numberOfLines,3))
+    classLabelVector=[]
+    index=0
+    for line in arrayOfLines:
+        line=line.strip()
+        listFromLine=line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index+=1
+    return returnMat,classLabelVector
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minVals, (m,1))
+    normDataSet=normDataSet/tile(ranges, (m,1))
+    return normDataSet,ranges,minVals
+def datingClassTest():
+    hoRatio=0.10
+    datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    m=normMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCount=0.0
+    for i in range(numTestVecs):
+        classifierResult=classifyO(normMat[i,:],normMat[numTestVecs:m,:],\
+                                   datingLabels[numTestVecs:m],3)
+        print ("the classifier came back with: %d,the real answer is: %d"\
+        %(classifierResult,datingLabels[i]))
+        if (classifierResult!=datingLabels[i]): errorCount+=1.0
+    print ("the total error rate is: %f" %(errorCount/float(numTestVecs)))
+def classifyPerson():
+    resultList=['not at all','in small','in large']
+    percentTats=float(input(\
+        "percentage of time?"))
+    ffMiles=float(input("frequent?"))
+    iceCream=float(input("liters of ?"))
+    dataMat,dataLabel=file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(dataMat)
+    inArr=array([ffMiles,percentTats,iceCream])
+    classifierResult=classifyO((inArr-\
+                                minVals)/ranges,normMat,dataLabel,3)
+    print ("person: ",resultList[classifierResult-1])
+def img2vector(filename):
+    returnVect=zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        lineStr=fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j]=int(lineStr[j])
+    return returnVect
+def handwritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('digits/trainingDigits')
+    m=len(trainingFileList)
+    trainingMat=zeros((m,1024))
+    for i in range(m):
+        fileNameStr=trainingFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('digits/trainingDigits/%s'%fileNameStr)
+    testFileList=listdir('digits/testDigits')
+    errorCount=0.0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        vectorUnderTest=img2vector('digits/testDigits/%s'%fileNameStr)
+        classifierResult=classifyO(vectorUnderTest, \
+                                   trainingMat, hwLabels,3)
+        print ("the classifier came back with: %d,the real answer is: %d"\
+               %(classifierResult,classNumStr))
+        if(classifierResult!=classNumStr):errorCount+=1.0
+    print ("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+def calcShannonEnt(dataSet):
+    numEntries=len(dataSet)
+    labelCounts={}
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=0
+        labelCounts[currentLabel]+=1
+    shannonEnt=0.0
+    for key in labelCounts:
+        prob=float(labelCounts[key])/numEntries
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
+def createDataSet():
+    dataSet=[
+        [1,1,'yes'],
+        [1,1,'yes'],
+        [1,0,'no'],
+        [0,1,'no'],
+        [0,1,'no']
+    ]
+    labels=['no surfacing','flipper']
+    return dataSet,labels
+def splitDataSet(dataSet,axis,value):
+    retDataSet=[]
+    for featVec in dataSet:
+        if featVec[axis]==value:
+            reducedFeatVec=featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures=len(dataSet[0])-1
+    baseEntropy=calcShannonEnt(dataSet)
+    bestInfoGain=0.0
+    bestFeature=-1
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+=prob*calcShannonEnt(subDataSet)
+        infoGain=baseEntropy-newEntropy
+        if(infoGain>bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+    return bestFeature
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote]=0
+        classCount[vote]+=1
+    sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount
+def createTree(dataSet,labels):
+    classList=[example[-1] for example in dataSet]
+    if classList.count(classList[0])==len(classList):
+        return classList[0]
+    if len(dataSet[0])==1:
+        return majorityCnt(classList)
+    bestFeat=chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    for value in uniqueVals:
+        subLabels=labels[:]
+        myTree[bestFeatLabel][value]=createTree(splitDataSet\
+            (dataSet,bestFeat,value),subLabels)
+    return myTree
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    return postingList,classVec
+def createVocabList(dataSet):
+    vocabSet=set([])
+    for document in dataSet:
+        vocabSet=vocabSet|set(document)
+    return list(vocabSet)
+def setOfWords2Vec(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]=1
+        else: print ("the word: %s"% word)
+    return returnVec
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs=len(trainMatrix)
+    numWords=len(trainMatrix[0])
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+    p0Num=ones(numWords);p1Num=ones(numWords)
+    p0Denom=2.0
+    p1Denom=2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+    p1Vect=zeros(numWords)
+    p0Vect=zeros(numWords)
+    for i in range(numWords):
+        p1Vect[i]=log(p1Num[i]/p1Denom)
+        p0Vect[i]=log(p0Num[i]/p0Denom)
+    return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+log(1.0-pClass1)
+    if(p1>p0):
+        return 1
+    else:
+        return 0
+def testingNB():
+    listOPosts,listClasses=loadDataSet()
+    myVocabList=createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb=trainNB0(array(trainMat),array(listClasses))
+    testEntry=['love','my','dalmation']
+    thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
+    print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry=['stupid','garbage']
+    thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
+    print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+def loadDataSet():
+    dataMat=[];labelMat=[];
+    fr=open('testSet.txt')
+    for line in fr.readlines():
+        lineArr=line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+def gradAscent(dataMatIn,classLabels):
+    dataMatrix=mat(dataMatIn)
+    labelMat=mat(classLabels).transpose()
+    m,n=shape(dataMatrix)
+    alpha=0.001
+    maxCycles=500
+    weights=ones((n,1))
+    for k in range(maxCycles)
+        h=sigmoid(dataMatrix*weights)
+        error=(labelMat-h)
+        weights=weights+alpha*dataMatrix.transpose()*error
+    return weights
+def loadDataSet(fileName):
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+
+def selectJrand(i,m):
+    j=i #we want to select any J not equal to i
+    while (j==i):
+        j = int(random.uniform(0,m))
+    return j
+
+def clipAlpha(aj,H,L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
+    retArray=ones((shape(dataMatrix)[0],1))
+    if threshIneq=='lt':
+        retArray[dataMatrix[:,dimen]<=threshVal]=-1.0
+    else:
+        retArray[dataMatrix[:,dimen]>threshVal]=-1.0
+    return retArray
+    labelMat=mat(classLabels).T
+def buildStump(dataArr,classLabels,D):
+    dataMatrix=mat(dataArr);
+    m,n=shape(dataMatrix)
+    numSteps=10.0;
+    bestStump={};
+    bestClasEst=mat(zeros((m,1)))
+    minError=inf
+    for i in range(n):
+        rangeMin=dataMatrix[:,i].min();
+        rangeMax=dataMatrix[:,i].max();
+        stepSize=(rangeMax-rangeMin)/numSteps
+        for j in range(-1,int(numSteps)+1):
+            for inequal in ['lt','gt']:
+                threshVal=(rangeMin+float(j)*stepSize)
+                predictedVals= \
+                    stumpClassify(dataMatrix,i,threshVal,inequal)
+                errArr=mat(ones((m,1)))
+                errArr[predictedVals==labelMat]=0
+                weightedError=D.T*errArr
+                print ("split: dim %d, thresh %.2f, thresh inequal: \
+                        %s, the weighted error is %.3f" %\
+                       (i,threshVal,inequal,weightedError))
+                if weightedError<minError:
+                    minError=weightedError
+                    bestClasEst=predictedVals.copy()
+                    bestStump['dim']=i
+                    bestStump['thresh']=threshVal
+                    bestStump['ineq']=inequal
+    return bestStump,minError,bestClasEst
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    weakClassArr=[]
+    m=shape(dataArr)[0]
+    D=mat(ones((m,1))/m)
+    aggClassEst=mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst=buildStump(dataArr,classLabels,D)
+        print("D:",D.T)
+        alpha=float(0.5*log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha']=alpha
+        weakClassArr.append(bestStump)
+        print("classEst: ",classEst.T)
+        expon=multiply(-1*alpha*mat(classLabels).T,classEst)
+        D=multiply(D,exp(expon))
+        D=D/D.sum()
+        aggClassEst+=alpha*classEst
+        print("aggClassEst: ",aggClassEst.T)
+        aggErrors=multiply(sign(aggClassEst)!=mat(classLabels).T,ones((m,1)))
+        errorRate=aggErrors.sum()/m
+        print("total error: ",errorRate,"\n")
+        if errorRate== 0.0:break
+    return weakClassArr
+def loadDataSet(fileName):
+    numFeat=len(open(fileName).readline().split('\t'))-1
+    dataMat=[];labelMat=[]
+    fr=open(fileName)
+    for line in fr.readlines():
+        lineArr=[]
+        curLine=line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+def standRegres(xArr,yArr):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    xTx=xMat.T*xMat
+    if linalg.det(xTx)==0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws=xTx.I*(xMat.T*yMat)
+    return ws
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    m=shape(xMat)[0]
+    weights=mat(eye((m)))
+    for j in range(m):
+        diffMat=testPoint-xMat[j,:]
+        weights[j,j]=exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx=xMat.T*(weights*xMat)
+    if linalg.det(xTx)==0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws=xTx.T*(xMat.T*(weights*yMat))
+    return testPoint * ws
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m=shape(testArr)[0]
+    yHat=zeros(m);
+    for i in range(m):
+        yHat[i]=lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+def regLeaf(dataSet):
+    return mean(dataSet[:,-1])
+def regErr(dataSet):
+    return var(dataSet[:,-1])*shape(dataSet)[0]
+def loadDataSet(fileName):
+    dataMat=[]
+    fr=open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine=map(float,curLine)
+        dataMat.append(fltLine)
+    return dataMat
+def binSplitDataSet(dataSet,feature,value):
+    mat0=dataSet[nonzero(dataSet[:,feature]>value)[0],:][0]
+    mat1=dataSet[nonzero(dataSet[:,feature]<=value)[0],:][0]
+    return mat0,mat1
+def chooseBestSplit(dataSet,leafType=regLeaf,errType=regErr,ops=(1,4)):
+    tolS=ops[0];tolN=ops[1]
+    if len(set(dataSet[:,-1].T.tolist()[0]))==1:
+        return None,leafType(dataSet)
+    m,n=shape(dataSet)
+    S=errType(dataSet)
+    bestS=inf;bestIndex=0;bestValue=0
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:,featIndex]):
+            mat0,mat1=binSplitDataSet(dataSet,featIndex,splitVal)
+            if (shape(mat0)[0]<tolN) or (shape(mat1)[0]<tolN): continue
+            newS=errType(mat0)+errType(mat1)
+            if newS<bestS:
+                bestIndex=featIndex
+                bestValue=splitVal
+                bestS=newS
+    if(S-bestS)<tolS:
+        return None,leafType(dataSet)
+    mat0,mat1=binSplitDataSet(dataSet,bestIndex,bestValue)
+    if (shape(mat0)[0]<tolN) or (shape(mat1)[0]<tolN):
+        return None,leafType(dataSet)
+    return bestIndex,bestValue
+def createTree(dataSet,leafType=regLeaf,errType=regErr,ops=(1,4)):
+    feat,val = chooseBestSplit(dataSet,leafType,errType,ops)
+    if feat==None:return val
+    retTree={}
+    retTree['spInd']=feat
+    retTree['spVal']=val
+    lSet,rSet=binSplitDataSet(dataSet,feat,val)
+    retTree['left']=createTree(lSet,leafType,errType,ops)
+    retTree['right']=createTree(rSet,leafType,errType,ops)
+    return retTree
+def loadDataSet(fileName):
+    dataMat=[]
+    fr=open(fileName)
+    for line in fr.readlines():
+        curLine=line.strip().split('\t')
+        fltLine=map(float,curLine)
+        dataMat.append(fltLine)
+    return dataMat
+def distEclud(vecA,vecB):
+    return sqrt(sum(power(vecA-vecB,2)))
+def randCent(dataSet,k):
+    n=shape(dataSet)[1]
+    centroids=mat(zeros((k,n)))
+    for j in range(n):
+        minJ=min(dataSet[:,j])
+        rangeJ=float(max(dataSet[:,j])-minJ)
+        centroids[:,j]=minJ+rangeJ*random.rand(k,1)
+    return centroids
+def kMeans(dataSet,k,distMeas=distEclud,createCent=randCent):
+    m=shape(dataSet)[0]
+    clusterAssment=mat(zeros((m,2)))
+    centroids=createCent(dataSet,k)
+    clusterChanged=True
+    while clusterChanged:
+        clusterChanged=False
+        for i in range(m):
+            minDist=inf;minIndex=-1;
+            for j in range(k):
+                distJI=distMeas(centroids[j,:],dataSet[i,:])
+                if distJI<minDist:
+                    minDist=distJI;minIndex=j;
+            if clusterAssment[i,0]!=minIndex:clusterChanged=True
+            clusterAssment[i,:]=minIndex,minDist**2
+        print (centroids)
+        for cent in range(k):
+            ptsInClust=dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
+            centroids[cent,:]=mean(ptsInClust,axis=0)
+    return centroids,clusterAssment
+def drop_first_last(grades):
+    first,*middle,last=grades
+    return math.avg(middle)
+def loadDataSet():
+    return [[1,3,4],[2,3,5],[1,2,3,5],[2,5]]
+def createC1(dataSet):
+    C1=[]
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+    C1.sort()
+    return map(frozenset,C1)
+def scanD(D,Ck,minSupport):
+    ssCnt={}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if not ssCnt.has_key(can):ssCnt[can]=1
+                else: ssCnt[can]+=1
+    numItems=float(len(D))
+    retList=[]
+    supportData={}
+    for key in ssCnt:
+        support=ssCnt[key]/numItems
+        if support>=minSupport:
+            retList.insert(0,key)
+        supportData[key]=support
+    return retList,supportData
