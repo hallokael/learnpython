4eda133b98a70c7fb94462ea2b83f873515deca1
hallokael <646058898@qq.com>
2017-10-29 23:41:25 +0800

    CustomClass

---
+++ learn6.py
@@ -0,0 +1,25 @@
+class Fib( object ):
+    def __init__(self  ):
+        self.a,self.b=0,1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b
+        if self.a>100000:
+            raise StopIteration()
+        return self.a
+    def __getitem__(self, item):
+        a,b=1,1
+        for x in range( item ):
+            a,b=b,a+b
+        return a
+class Student( object ):
+    def __getattr__(self, item):
+        if item=='age':
+            return lambda : 22
+        if item=='score':
+            return 99
+f=Fib()
+s=Student()
+print( f[5],f[6] )
+print( s.age(),s.score )
