b4e56a60b8d8b6ff060de3970cc781887fecae8a
hallokael <646058898@qq.com>
2017-10-29 19:38:15 +0800

    class private

--- learn4.py
+++ learn4.py
@@ -1,10 +1,11 @@
 class Student( object ):
     def __init__(self,name,score):
-        self.name=name
-        self.score=score
+        self.__name=name
+        self.__score=score
     def print_score(self ):
-        print('%s: %s'%( self.name,self.score ))
+        print('%s: %s'%( self.__name,self.__score ))
 bart=Student( 'Bart Simp',59 )
 lisa=Student( 'Lisa Simp',87 )
 bart.print_score()
-lisa.print_score()
+print( bart._Student__name )
+bart.__name
