16f17dea65769abf7dee6ace7fc0b5e0725d203b
hallokael <646058898@qq.com>
2017-10-29 13:45:16 +0800

    sorted

--- 10.30learn.py
+++ 10.30learn.py
@@ -10,12 +10,28 @@ print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
 def str2float(s):
     def ch2num(s):
         if s!='.':
-            print(s,"zzz")
             return int( s )
         else: return s
-       # return int( s) if s!='.' else s
     def Mul( x,y ):
         return x*10+y if y!='.' else x
     return reduce( Mul,map( ch2num,s ) )/10**( len( s )-s.find('.')-1 )
-
 print('str2float(\'12345.678\') =', str2float('12345.678'))
+
+
+
+A=[36, 5, -12, 9, -21]
+print(sorted( A,key=abs ))
+print(sorted( A,key=lambda x:-x if x<0 else x))
+print( sorted( ['bob','about','Zoo','Credit'],key=str.lower ) )
+
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[0]
+def by_score( t ):
+    return -t[1]
+L2 = sorted(L, key=by_name)
+print(L2)
+
+L2 = sorted(L, key=by_score)
+print(L2)
