34f2c0483075dd2bf875592e4176af57f7c2ebdc
hallokael <646058898@qq.com>
2017-10-30 16:08:39 +0800

    IO

---
+++ learn9.py
@@ -0,0 +1,16 @@
+with open('tee','r') as f:
+    print(f.readline())
+with open('tee','w') as f:
+    f.write('hello,world!')
+from io import StringIO
+f=StringIO( 'hello\nhi\nbye' )
+while True:
+    s=f.readline()
+    if s== '':
+        break
+    print( s.strip() )
+import os
+print( os.name,os.environ )
+print( [x for x in os.listdir('.') if os.path.isdir(x)] )
+print( [x for x in os.listdir('.')] )
+print( [x for x in os.listdir('.') if x[-3:]=='.py' ] )
