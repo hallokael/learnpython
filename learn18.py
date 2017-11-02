d3a724be0d4dd4749cdf149ff5fdf90287d78bb5
hallokael <646058898@qq.com>
2017-11-02 15:44:03 +0800

    asyncio

---
+++ learn18.py
@@ -0,0 +1,31 @@
+import asyncio
+@asyncio.coroutine
+def hello():
+    print('hello')
+    r=yield from asyncio.sleep(1)
+    print("hello again")
+#loop=asyncio.get_event_loop()
+#loop.run_until_complete(hello())
+#loop.close()
+@asyncio.coroutine
+def wget(host):
+    print('wget %s'%host)
+    connect=asyncio.open_connection(host,80)
+    print('where-1' + host)
+    reader,writer=yield from connect
+    print('where0' + host)
+    header='GET / HTTP/1.0\r\nHost:%s\r\n\r\n'%host
+    writer.write(header.encode('utf-8'))
+    print('where1'+host)
+    yield from writer.drain()
+    print('where2'+host)
+    while True:
+        line=yield from reader.readline()
+        if line==b'\r\n':
+            break
+        print('%s header > %s'%(host,line.decode('utf-8').rstrip()))
+    writer.close()
+loop=asyncio.get_event_loop()
+tasks=[wget(host) for host in ['www.sina.com.cn','www.sohu.com','www.163.com']]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
