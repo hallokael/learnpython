045706341dd55f110ccd703c88e8eb66550df4e3
hallokael <646058898@qq.com>
2017-11-02 16:24:26 +0800

    asyncio2

--- learn18.py
+++ learn18.py
@@ -25,7 +25,24 @@ def wget(host):
             break
         print('%s header > %s'%(host,line.decode('utf-8').rstrip()))
     writer.close()
+async def wget2(host):
+    print('wget %s'%host)
+    connect=asyncio.open_connection(host,80)
+    print('where-1' + host)
+    reader,writer=await connect
+    print('where0' + host)
+    header='GET / HTTP/1.0\r\nHost:%s\r\n\r\n'%host
+    writer.write(header.encode('utf-8'))
+    print('where1'+host)
+    await writer.drain()
+    print('where2'+host)
+    while True:
+        line=await reader.readline()
+        if line==b'\r\n':
+            break
+        print('%s header > %s'%(host,line.decode('utf-8').rstrip()))
+    writer.close()
 loop=asyncio.get_event_loop()
-tasks=[wget(host) for host in ['www.sina.com.cn','www.sohu.com','www.163.com']]
+tasks=[wget2(host) for host in ['www.sina.com.cn','www.sohu.com','www.163.com']]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
